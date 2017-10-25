708c3c8bbe2a8e7ab27559e32f14264c145fc006
tomatehk <ventoryoos@gmail.com>
2017-10-25 11:37:30 -0400

    Update wsgi.py

--- ventoryos/wsgi.py
+++ ventoryos/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "inventario.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "ventoryos.settings"
 
 application = get_wsgi_application()
